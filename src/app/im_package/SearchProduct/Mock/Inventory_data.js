e922718d04e427df4dc67b9cfca0865398069a53
DinhNguyen24 <nguyenqert24@gmail.com>
2024-10-26 16:14:48 +0700

    fix

--- src/app/im_package/SearchProduct/Mock/Inventory_data.js
+++ src/app/im_package/SearchProduct/Mock/Inventory_data.js
@@ -4,12 +4,6 @@ export const Inventory_dataMock = {
             id: 1,
             maHang: 'H001',
             tenHang: 'Sản phẩm A',
-            nhaCungCap: {
-              maNCC: 'DL001',
-              tenNCC: 'Đại lý 1',
-              diaChi: 'Địa chỉ 1',
-              soDienThoai: '0123456789'
-            },
             soLuong: 10,
             gia: 20000,
           },
@@ -17,12 +11,6 @@ export const Inventory_dataMock = {
             id: 2,
             maHang: 'H002',
             tenHang: 'Sản phẩm B',
-            nhaCungCap: {
-              maNCC: 'DL002',
-              tenNCC: 'Đại lý 2',
-              diaChi: 'Địa chỉ 2',
-              soDienThoai: '0987654321'
-            },
             soLuong: 5,
             gia: 15000,
           },
@@ -30,12 +18,6 @@ export const Inventory_dataMock = {
             id: 3,
             maHang: 'F002',
             tenHang: 'Sản phẩm C',
-            nhaCungCap: {
-              maNCC: 'DL003',
-              tenNCC: 'Đại lý 3',
-              diaChi: 'Địa chỉ 3',
-              soDienThoai: '0987654321'
-            }, 
             soLuong: 18,
             gia: 16000,
           },
